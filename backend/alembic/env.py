46fec4559e944d6320fcaa7c116f1aa42b540c24
RKprogs <rkkumarmv@gmail.com>
2026-02-22 20:09:39 +0530

    Modified Database URL

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -13,6 +13,10 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+config.set_main_option(
+    "sqlalchemy.url",
+    os.getenv("DATABASE_URL") # type: ignore
+)
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
